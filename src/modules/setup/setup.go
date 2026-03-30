d5844bf28abed8cfa69ba12f1fe4550c11086ae8
DaanSelen <dselen@systemec.nl>
2026-03-30 13:37:18 +0200

    chore: rename artifacts

--- src/modules/setup/setup.go
+++ src/modules/setup/setup.go
@@ -1,14 +1,32 @@
 package setup
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 func PrepareEnvironment() (bool, error) {
-	if _, err := os.Stat("./bin"); err != nil {
-		return false, err
+	ok, err := fsCheck()
+
+	return ok, err
+}
+
+func fsCheck() (bool, error) {
+	dirs := []string{"./bin", "./books", "./history"}
+	files := []string{"./api.conf", "./os_categories.json"}
+
+	for _, d := range dirs {
+		if _, err := os.Stat(d); err != nil {
+			log.Printf("Unable to stat dir: %s", d)
+			return false, err
+		}
 	}
 
-	if _, err := os.Stat("./books"); err != nil {
-		return false, err
+	for _, f := range files {
+		if _, err := os.Stat(f); err != nil {
+			log.Printf("Unable to stat file: %s", f)
+			return false, err
+		}
 	}
 
 	return true, nil
